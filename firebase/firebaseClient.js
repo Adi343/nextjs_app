00e01749137598e0979299c7c59e4c8e34002742
Adi343 <53225414+Adi343@users.noreply.github.com>
2021-11-03 08:55:03 +0530

    config removed

--- firebase/firebaseClient.js
+++ firebase/firebaseClient.js
@@ -1,12 +1,6 @@
 import firebase from "firebase";
 import config from "./firebaseConfig";
 
-// Configure Firebase.
-const config1 = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  // ...
-};
 //Check if firebase is already initialized
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
